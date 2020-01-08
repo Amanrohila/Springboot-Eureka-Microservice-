0c32d1a854af491e3590364a45e69167159bbb52
amanrohila <amanrohila66@gmail.com>
2020-01-08 15:42:13 +0530

    basic project

---
+++ Springboot-Eureka-Microservice-demo/src/test/java/com/aman/eurekadiscoveryclient/demo/SpringbootEurekaMicroservicedemo/SpringbootEurekaMicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.eurekadiscoveryclient.demo.SpringbootEurekaMicroservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEurekaMicroserviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
